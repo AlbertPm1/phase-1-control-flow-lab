b32734592fd35a634e5755696a3f664058ca7a4b
Albertpm1 <petermburunganga98@gmail.com>
2022-11-26 22:00:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(lift){
+  if (lift <=400){
+  return 'This one is on me!'
+  }else if(lift>2000 && lift >2500){
+  return 'No can do.'
+  }else{
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city ==='NYC'? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.'
+
+    case 'not as generous':
+      return "Thank you."
+
+      default: 
+        return'Bye.' 
+  }
 }
